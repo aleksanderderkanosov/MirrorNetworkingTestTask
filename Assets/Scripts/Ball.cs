2caabd431e62f5845edde091c12148fd1745121d
derkanosov <derkanosov@nanosemantics.ai>
2024-08-23 22:42:05 +0300

    Add destroy sleeping balls

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,8 +1,41 @@
+using Mirror;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class Ball : NetworkBehaviour
 {
+    [SerializeField] private Rigidbody _ballRb;
+    [SerializeField] private float _timeToDestroy;
+
+    private bool _isSleeping = false;
+    private float _timerToDestroy = 0.0f;
     private NetworkPlayer _owner;
 
     public NetworkPlayer Owner { get => _owner; set => _owner = value; }
+
+    private void FixedUpdate() {
+        if (IsReadyToDestroy()) {
+            DestroyBall();
+        }
+    }
+
+    private bool IsReadyToDestroy() {
+        if (!isServer) {
+            return false;
+        }
+
+        if (_isSleeping != _ballRb.IsSleeping()) {
+            _isSleeping = !_isSleeping;
+            _timerToDestroy = 0.0f;
+        }
+
+        if (_isSleeping) {
+            _timerToDestroy += Time.fixedDeltaTime;
+        }
+
+        return _timerToDestroy >= _timeToDestroy;
+    }
+
+    private void DestroyBall() {
+        NetworkServer.Destroy(gameObject);
+    }
 }
